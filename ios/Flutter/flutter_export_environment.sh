e559aaecc3929a95e0e184d60635ae0bae53f403
ahmedtharwat19 <ahmed.tharwat19@gmail.com>
2026-07-16 13:57:18 +0300

    chore: تنظيف التاريخ من ملفات البناء

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=D:\SDKs\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Projects\purchasing"
-export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=D:\Projects\purchasing\ios\Flutter\ephemeral\Packages\.packages\FlutterFramework"
+export "FLUTTER_APPLICATION_PATH=D:\Projects\supplyChain-mirror"
+export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=D:\Projects\supplyChain-mirror\ios\Flutter\ephemeral\Packages\.packages\FlutterFramework"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=D:\SDKs\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Projects\purchasing"
-export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=D:\Projects\purchasing\macos\Flutter\ephemeral\Packages\.packages\FlutterFramework"
+export "FLUTTER_APPLICATION_PATH=D:\Projects\supplyChain-mirror"
+export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=D:\Projects\supplyChain-mirror\macos\Flutter\ephemeral\Packages\.packages\FlutterFramework"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.7.4"
